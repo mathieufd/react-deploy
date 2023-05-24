8e648b186150803b81b2942f4ab7285e6bb85c27
Mat <matmaximee@gmail.com>
2023-05-24 15:48:33 +0200

    first commit

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <div className="footer">
+    <span>
+      Made by
+      &nbsp;
+      <a href="https://thenextvideo.com" target="_blank">
+        Thenextvideo
+      </a>
+      &nbsp;
+      with
+      &nbsp;
+      <i className="fa fa-heart" />
+    </span>
+  </div>
+);
+
+export default Footer;
